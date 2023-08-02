ad511f87c11929d4f4526ceca7c9b9f6457b4854
Valeriano A.R <valen.alfonso@protonmail.com>
2023-08-02 12:01:59 +0200

    Delete unused methods from MessageBoxEx.

--- UI/MessageBoxEx.cs
+++ UI/MessageBoxEx.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Drawing;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace CsvView.UI
 {
     public static class MessageBoxEx
     {
-        private static IWin32Window _owner;
-
         public static DialogResult Show(string text)
         {
             Initialize();
@@ -47,83 +42,45 @@ namespace CsvView.UI
 
         public static DialogResult Show(IWin32Window owner, string text)
         {
-            _owner = owner;
             Initialize();
             return MessageBox.Show(owner, text);
         }
 
         public static DialogResult Show(IWin32Window owner, string text, string caption)
         {
-            _owner = owner;
             Initialize();
             return MessageBox.Show(owner, text, caption);
         }
 
         public static DialogResult Show(IWin32Window owner, string text, string caption, MessageBoxButtons buttons)
         {
-            _owner = owner;
             Initialize();
             return MessageBox.Show(owner, text, caption, buttons);
         }
 
         public static DialogResult Show(IWin32Window owner, string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon)
         {
-            _owner = owner;
             Initialize();
             return MessageBox.Show(owner, text, caption, buttons, icon);
         }
 
         public static DialogResult Show(IWin32Window owner, string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon, MessageBoxDefaultButton defButton)
         {
-            _owner = owner;
             Initialize();
             return MessageBox.Show(owner, text, caption, buttons, icon, defButton);
         }
 
         public static DialogResult Show(IWin32Window owner, string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon, MessageBoxDefaultButton defButton, MessageBoxOptions options)
         {
-            _owner = owner;
             Initialize();
             return MessageBox.Show(owner, text, caption, buttons, icon,
                                    defButton, options);
         }
         
-        [DllImport("user32.dll")]
-        private static extern bool GetWindowRect(IntPtr hWnd, ref Rectangle lpRect);
-
-        [DllImport("user32.dll")]
-        private static extern int MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
-
         private static void Initialize()
         {
 
         }
         
-        private static void CenterWindow(IntPtr hChildWnd)
-        {
-            Rectangle recChild = new Rectangle(0, 0, 0, 0);
-			GetWindowRect(hChildWnd, ref recChild);
-
-            int width = recChild.Width - recChild.X;
-            int height = recChild.Height - recChild.Y;
-
-            Rectangle recParent = new Rectangle(0, 0, 0, 0);
-			GetWindowRect(_owner.Handle, ref recParent);
-
-            Point ptCenter = new Point(0, 0);
-            ptCenter.X = recParent.X + ((recParent.Width - recParent.X) / 2);
-            ptCenter.Y = recParent.Y + ((recParent.Height - recParent.Y) / 2);
-
-
-            Point ptStart = new Point(0, 0);
-            ptStart.X = (ptCenter.X - (width / 2));
-            ptStart.Y = (ptCenter.Y - (height / 2));
-
-            ptStart.X = (ptStart.X < 0) ? 0 : ptStart.X;
-            ptStart.Y = (ptStart.Y < 0) ? 0 : ptStart.Y;
-
-			MoveWindow(hChildWnd, ptStart.X, ptStart.Y, width, height, false);
-        }
-
     }
 }
