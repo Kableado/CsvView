64e380926251130869829423c31d29f492efde08
Valeriano A.R <valen.alfonso@protonmail.com>
2023-08-05 15:40:12 +0200

    Add AvaloniaUI App project.

---
+++ CsvView/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace CsvView;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
